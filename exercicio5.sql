700bd47a7a55f52de29b4337eca8fac78806a4ca
Gabriel Farias <36546452+BielSousa@users.noreply.github.com>
2021-08-16 14:56:16 -0300

    Delete exercicio5.sql

--- exercicio5.sql
+++
@@ -1,6 +0,0 @@
-/*
-Encontre os clientes do estado de SP e que não morem na cidade de São Paulo.
-*/
-SELECT * 
-FROM olist_customers_dataset 
-WHERE (customer_state = "SP" and customer_city != "sao paulo");
