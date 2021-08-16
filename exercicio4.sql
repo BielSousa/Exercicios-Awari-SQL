8c90b700443f05a6d84158c27b04086a7312ddc7
Gabriel Farias <36546452+BielSousa@users.noreply.github.com>
2021-08-16 14:55:57 -0300

    Delete exercicio4.sql

--- exercicio4.sql
+++
@@ -1,7 +0,0 @@
-/*
-Retorne pedidos que foram feitos somente no ano de 2017.
-*/
-SELECT * 
-from olist_orders_dataset 
-where order_purchase_timestamp 
-	BETWEEN "2017-01-01 00:00" and "2017-12-31 23:59";
