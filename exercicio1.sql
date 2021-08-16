1d44aa1eab17875511cbc66f4a9aec6f5a8432fc
Gabriel Farias <36546452+BielSousa@users.noreply.github.com>
2021-08-16 14:55:11 -0300

    Delete exercicio1.sql

--- exercicio1.sql
+++
@@ -1,7 +0,0 @@
-/*
-Selecione os dados da tabela de pagamentos onde só apareçam os tipos de pagamento “VOUCHER” e “BOLETO”.
-*/
-
-SELECT * 
-FROM olist_order_payments_dataset 
-WHERE (payment_type = "boleto" or payment_type = "voucher");
