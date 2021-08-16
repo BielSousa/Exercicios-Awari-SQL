a39f92a4efd870e2723f7d7c74d540049c11ac90
Gabriel Farias <36546452+BielSousa@users.noreply.github.com>
2021-08-16 14:55:42 -0300

    Delete exercicio2.sql

--- exercicio2.sql
+++
@@ -1,9 +0,0 @@
-/*
-Retorne os campos da tabela de produtos e calcule o volume de cada produto em um novo campo.
-*/
-
-SELECt 
-	olist_products_dataset.product_id, 
-	SUM((product_height_cm*product_length_cm*product_width_cm)) AS Volume 
-FROM olist_products_dataset 
-GROUP BY product_id;
